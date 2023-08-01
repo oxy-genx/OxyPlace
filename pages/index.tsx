23eb5d4087013ef320544c06edafb3e5c26ed2df
yashdeharia <yashdeharia2003@gmail.com>
2023-08-01 23:56:01 +0530

    add next/img in index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Link from "next/link";
-// import Image from "next/image";
+import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 /**
